d4ca7731b8f52ac5ff8132b0d6a2e0f0d1aecf82
Khan Mohsin <m90khan@gmail.com>
2020-08-11 06:00:23 +0500

    update

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,3 +2,7 @@ import "../sass/main.scss";
 
 import { test } from "./test/index";
 console.log(test);
+alert(2);
+if (module.hot) {
+  module.hot.accept();
+}
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,4 +1,5 @@
-const path = require("path"); // * to generate absolute path to the current flder
+const path = require("path");
+// * to generate absolute path to the current flder
 
 const postCSSPlugins = [require("autoprefixer")];
 
@@ -8,9 +9,17 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist/"),
   },
-  mode: "development",
-  watch: true,
 
+  devServer: {
+    before: function (app, server) {
+      server._watch("./**/*.html");
+    },
+    contentBase: path.resolve(__dirname, "./"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  mode: "development",
   module: {
     rules: [
       {
